8268ae2ed61ddbe3d319db954999191df4455f16
nodaro.ho <nodaro.ho@u-pec.fr>
2020-10-26 17:45:40 +0100

    Manage exception when a username doesn't exists

--- scrapper.py
+++ scrapper.py
@@ -7,17 +7,21 @@ class Scrapper:
         self.config.Pandas=True
 
     def scrapp_tweets(self, username, limit, since=None, until=None):
-        self.config.Username = username
-        self.config.Limit = limit
-        self.config.Since = since
-        self.config.Until = until
-        twint.run.Search(self.config)
-        tweets_df = twint.storage.panda.Tweets_df
-        print(tweets_df.columns)
-        print(tweets_df['tweet'])
-        return tweets_df
+        try:
+            self.config.Username = username
+            self.config.Limit = limit
+            self.config.Since = since
+            self.config.Until = until
+            twint.run.Search(self.config)
+            tweets_df = twint.storage.panda.Tweets_df
+            print(tweets_df.columns)
+            print(tweets_df['tweet'])
+            return tweets_df
+        except KeyError as e:
+            print("Username: "+ username + " doesn't exists on Twitter " + str(e))
+            return None
 
 
 if __name__ == "__main__":
     scrapper = Scrapper()
-    scrapper.scrapp_tweets("realDonaldTrump", 10)
+    scrapper.scrapp_tweets("aioncamghgri", 10)
