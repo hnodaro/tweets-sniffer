5985bd7d6581017235e19e8a3b258968ba0813d0
nodaro.ho <nodaro.ho@u-pec.fr>
2020-10-28 19:47:22 +0100

    Send data from UI to back-end

--- tweets_scrapper/routes.py
+++ tweets_scrapper/routes.py
@@ -1,8 +1,13 @@
 from tweets_scrapper import app
-from flask import render_template, redirect, url_for, request
-from tweets_scrapper.form import Form
+from flask import render_template, redirect, url_for, request, request
 from tweets_scrapper.scrapper import Scrapper
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == "POST":
+        username = request.form.get("username")
+        limit  = request.form.get("limit")
+        # scrapp = Scrapper()
+        # scrapp.scrapp_tweets(username, limit)
+        return redirect(request.url)
     return render_template('index.html')
