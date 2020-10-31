303b1bfc51630d6a037072ed95e0057c0dc76f87
hnodaro <hnodaro@gmail.com>
2020-10-31 12:37:41 +0100

    Correct bug: when res is not a DaraFrame

--- tweets_scrapper/routes.py
+++ tweets_scrapper/routes.py
@@ -1,6 +1,7 @@
 from tweets_scrapper import app
 from flask import render_template, redirect, url_for, request, request
 from tweets_scrapper.scrapper import Scrapper
+import pandas as pd
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -10,7 +11,7 @@ def home():
         limit = int(limit) if limit else None
         scrapp = Scrapper()
         res = scrapp.scrapp_tweets(username, limit)
-        if res:
+        if isinstance(res, pd.DataFrame):
             res = res[['date','tweet','nlikes','nreplies','nretweets']]
             res = res.rename(columns={"date": "Date", "tweet": "Tweet", "nlikes": "Number of likes", "nreplies": "Number of replies", "nretweets": "Number of retweets"})
             return render_template('result.html', username=username, tables=[res.to_html(classes='table table-striped text-center',index = False, header="true")])
